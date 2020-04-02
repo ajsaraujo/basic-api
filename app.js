2c010d2b542439bce5b84c745eee43b5503621a6
Allan <allanjuansil@gmail.com>
2020-04-02 10:23:29 -0300

    Configurar .env e conectar com banco de dados
    Instalamos o mongoose e o .env no projeto. O primeiro permite que conectemos ao banco com facilidade, enquanto o segundo serve pra administrar
    variáveis de ambiente, facilitando o desenvolvimento e protegendo informações sensíveis do nosso projeto.

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const express = require('express');
-const app = express();
+require('dotenv').config({
+    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+}); 
 
 const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose'); 
+const mongooseOptions = { useNewUrlParser: true, useUnifiedTopology: true }; 
+const connectionString = process.env.DB_HOST + '/' + process.env.DB_NAME; 
+
+console.log(connectionString);
+mongoose.connect(connectionString, mongooseOptions);
+
+mongoose.connection.once('open', _ => {
+    console.log('Aplicação conectada ao banco de dados.'); 
+});
+
+mongoose.connection.on('error', err => {
+    console.log(`ERRO: Conexão com o banco de dados falhou.`);
+    console.log(err); 
+});
+
+const express = require('express');
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,7 +32,7 @@ const userRoute = require('./routes/user');
 app.use('/', indexRoute);
 app.use('/user', userRoute);
 
-app.listen(3000);
-console.log('A API está escutando na porta 3000.');
+app.listen(process.env.PORT);
+console.log(`A API está escutando na porta ${process.env.PORT}.`);
 
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {MongoClient} = require('mongodb'); 
 
 router.get('/', (req, res) => {
 	return res.send({ message: 'Listando todos os usuários!' });
@@ -7,9 +8,22 @@ router.get('/', (req, res) => {
 
 router.post('/create', (req, res) => {
 	const { email, password } = req.body;
-	if (!email || !password) return res.status(400).send({ error: 'Requisição com dados insuficientes.'});
+
+	if (!email || !password) {
+		return res.status(400).send({ error: 'Requisição com dados insuficientes.'});
+	}
 
 	return res.send({ message: `Criando usuário com email ${email} e senha ${password}` });
+});
+
+router.post('/auth', (req, res) => {
+	const { email, password } = req.body; 
+
+	if (!email || !password) {
+		return res.status(400).send({ error: 'Requisição com dados insuficientes.'});
+	}
+
+	return res.send({ message: 'Usuário autenticado.'}); 
 });
 
 module.exports = router;
