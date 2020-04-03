fa281db1443719c7eaa65ccd79ae2dc9cf0f2538
Allan <allanjuansil@gmail.com>
2020-04-02 21:17:33 -0300

    Criar rota de atualização de dados do usuário

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,20 +1,28 @@
 const User = require('../models/User'); 
 const yup = require('yup'); 
 
+const createSchema = yup.object().shape(
+    {
+       name: yup.string().required(), 
+       email: yup.string().required().email(), 
+       password: yup.string().required()
+    }
+);
+
+const updateSchema = yup.object().shape(
+    {
+        name: yup.string(),
+        email: yup.string().required().email(),
+        password: yup.string()
+    }
+);
+
 class UserController {
 
     async create(req, res) {
         const email = req.body.email; 
 
-        const schema = yup.object().shape(
-            {
-               name: yup.string().required(), 
-               email: yup.string().required().email(), 
-               password: yup.string().required()
-            }
-        );
-
-        let requestBodyIsValid = schema.isValid(req.body); 
+        let requestBodyIsValid = createSchema.isValid(req.body); 
         
         if (!requestBodyIsValid) {
             return res.status(400).json({ error: 'Requisição com corpo inválido.'});
@@ -30,6 +38,36 @@ class UserController {
         newUser.password = undefined; 
 
         return res.status(201).json(newUser); 
+    }
+
+    async update(req, res) {
+        const { email, name, password } = req.body; 
+
+        let requestBodyIsValid = updateSchema.isValid(req.body); 
+
+        if (!requestBodyIsValid) {
+            return res.status(400).json({ error: 'Requisição com corpo inválido.' }); 
+        }
+
+        let user = await User.findOne({ email }); 
+
+        if (!user) {
+            return res.status(409).json({ error: 'O e-mail informado não está associado a nenhuma conta.'});
+        }
+        
+        if (name) {
+            user.name = name; 
+        }
+
+        if (password) {
+            user.password = password; 
+        }
+
+        await user.save(); 
+        
+        user.password = undefined; 
+        
+        return res.json(user); 
     }
 }
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', UserController.create); 
-//router.put('/', UserController.update); 
+router.put('/', UserController.update); 
 //router.delete('/', UserController.delete); 
 
 module.exports = router;
