90995de2a9ddbc81f2322becf6e230b30b897e1c
Allan <allanjuansil@gmail.com>
2020-04-22 20:05:29 -0300

    Adicionar testes unitários para middleware de email

--- src/tests/unit/middlewares.test.js
+++ src/tests/unit/middlewares.test.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 
 const httpMocks = require('node-mocks-http');
-const auth = require('../../middlewares/auth');
 const jwt = require('jsonwebtoken');
 
+const User = require('../../models/User');
+const auth = require('../../middlewares/auth');
+const emailInUse = require('../../middlewares/emailInUse');
+
 jest.mock('jsonwebtoken');
+jest.mock('../../models/User');
 
 let req, res, next; 
 
@@ -14,7 +18,7 @@ beforeEach(() => {
     next = jest.fn();
 });
 
-describe('auth middleware', () => {
+describe('auth', () => {
     beforeEach(() => {
         req.headers.auth = 'some_token';
         req.params.userId = 'rgfh4hs6hfh54sg46';
@@ -36,6 +40,29 @@ describe('auth middleware', () => {
         req.headers.auth = 'reittiwerpirwr786deg6s';
         jwt.verify.mockReturnValue({ id: 'rgfh4hs6hfh54sg46'});
         await auth(req, res, next);
+        expect(next).toBeCalled();
+    });
+});
+
+describe('email in use', () => {
+    beforeEach(() => {
+        req.body.email = 'johndoe@gmail.com';
+    });
+    it('should return 400 if request body doesn\'t have an email field', async () => {
+        req.body.email = undefined;
+        await emailInUse(req, res, next);
+        expect(res.statusCode).toBe(400);
+    });
+    it('should return 500 if error is thrown', async () => {
+        User.find.mockImplementation(() => {
+            throw new Error();
+        });
+        await emailInUse(req, res, next);
+        expect(res.statusCode).toBe(500);
+    });
+    it('should call next if everything goes fine', async () => {
+        User.find.mockReturnValue([1]);
+        await emailInUse(req, res, next);
         expect(next).toBeCalled();
     });
 });
